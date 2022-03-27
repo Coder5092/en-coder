a655daf2bcb74f0303efbd72a7ae052b7a9a32c0
Coder5092 <101958748+Coder5092@users.noreply.github.com>
2022-03-26 18:52:33 -0700

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,15 +1,14 @@
-
-ipts = input("String to encode: ")
-cr = {}
-chars = 'qwertyuioplkjhgfdsazxcvbnm`1234567890-=[]\;\',./~!@#$%^&*()_+{}|:"<>?QWERTYUIOPLKJHGFDSAZXCVBNM'
-for char in ipts:
-  if char not in chars:
-    print('Character not supported: ' + char)
-    while 1:
-      pass
-for char in chars:
-  cr[char] = chars[(chars.index(char) + 5) % (len(chars) - 1)]
-out = ""
-for char in ipts:
-  out += cr[char]
-print(out)
+def encode(ipts):
+  cr = {}
+  chars = 'qwertyuioplkjhgfdsazxcvbnm`1234567890-=[]\;\',./~!@#$%^&*()_+{}|:"<>?QWERTYUIOPLKJHGFDSAZXCVBNM'
+  for char in ipts:
+    if char not in chars:
+      print('Character not supported: ' + char)
+      while 1:
+        pass
+  for char in chars:
+    cr[char] = chars[(chars.index(char) + 5) % (len(chars) - 1)]
+  out = ""
+  for char in ipts:
+    out += cr[char]
+  print(out)
